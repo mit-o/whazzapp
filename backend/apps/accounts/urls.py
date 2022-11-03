4c4fd24c1f6f634053b69d6276203a10e9705287
Sebastian Tarczyński <starczynski7@gmail.com>
2022-11-03 22:30:48 +0100

    add endpoints for current user and listing all users

--- backend/apps/accounts/urls.py
+++ backend/apps/accounts/urls.py
@@ -1,9 +1,17 @@
 from django.urls import include, path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenBlacklistView
+from rest_framework.routers import DefaultRouter
 from . import views
 
+router = DefaultRouter()
+router.register(r"users", views.UserViewSet, basename="users")
+
 urlpatterns = [
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/", views.CustomTokenObtainView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/blacklist/", TokenBlacklistView.as_view(), name="token_blacklist"),
     path("register/", views.RegisterView.as_view(), name="register"),
+    path("me/", views.CurrentUserView.as_view(), name="current_user"),
 ]
+
+urlpatterns += router.urls
--- backend/apps/accounts/views.py
+++ backend/apps/accounts/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth import get_user_model
 from rest_framework import generics, permissions, status
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import RetrieveModelMixin, ListModelMixin, UpdateModelMixin
+from rest_framework.decorators import action
 from rest_framework.response import Response
-
-from .serializers import RegisterSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import RegisterSerializer, CustomTokenObtainSerializer, UserSerializer
 
 User = get_user_model()
 
@@ -23,3 +26,39 @@ class RegisterView(generics.CreateAPIView):
         return Response(
             serializer.data, status=status.HTTP_201_CREATED, headers=headers
         )
+
+
+class UserViewSet(RetrieveModelMixin, ListModelMixin, GenericViewSet):
+    queryset = User.objects.all()
+
+    @action(detail=False)
+    def all(self, request):
+        serializer = UserSerializer(
+            User.objects.all(), many=True, context={"request": request}
+        )
+        return Response(status=status.HTTP_200_OK, data=serializer.data)
+
+
+class CurrentUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def retrieve(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
+
+    def update(self, request):
+        serializer = self.serializer_class(request.user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request):
+        serializer = self.serializer_class(
+            request.user, data=request.data, partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
