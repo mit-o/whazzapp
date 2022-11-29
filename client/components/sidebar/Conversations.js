d79f9a2d6c50b5d0c66fe2c7f9bacf16ab369211
Sebastian Tarczyński <starczynski7@gmail.com>
2022-11-29 04:29:05 +0100

    set global styling for scrollbar

--- client/components/sidebar/Conversations.js
+++ client/components/sidebar/Conversations.js
@@ -26,39 +26,16 @@ const Conversations = () => {
   }, [user]);
 
   return (
-    <>
-      <div className="flex flex-col py-3 text-base overflow-y-scroll scrollbar">
-        {activeConversations?.map((conversation) => (
-          <Conversation
-            key={conversation.id}
-            click={() => dispatch(setActiveChat(conversation))}
-            participants={conversation.participants}
-            lastMessage={conversation.last_message}
-          />
-        ))}
-      </div>
-      <style jsx>
-        {`
-          .scrollbar::-webkit-scrollbar {
-            width: 8px;
-          }
-
-          .scrollbar::-webkit-scrollbar-track {
-            background-color: #233138;
-            border-radius: 10px;
-          }
-
-          .scrollbar::-webkit-thumb {
-            background-color: #121b21;
-            border-radius: 10px;
-          }
-
-          .scrollbar::-webkit-scrollbar-thumb:hover {
-            background-color: #121b21;
-          }
-        `}
-      </style>
-    </>
+    <div className="flex flex-col py-3 text-base overflow-y-scroll scrollbar">
+      {activeConversations?.map((conversation) => (
+        <Conversation
+          key={conversation.id}
+          participants={conversation.participants}
+          lastMessage={conversation.last_message}
+          click={() => dispatch(setActiveChat(conversation))}
+        />
+      ))}
+    </div>
   );
 };
 
