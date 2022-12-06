1962b1feaaf4cf58b3f91f81e543c62e36a42850
Sebastian Tarczyński <starczynski7@gmail.com>
2022-12-06 17:15:08 +0100

    fix permission

--- backend/apps/accounts/permissions.py
+++ backend/apps/accounts/permissions.py
@@ -2,5 +2,5 @@ from rest_framework import permissions
 
 
 class IsOwnerOrAdmin(permissions.BasePermission):
-    def has_object_permission(self, request, obj):
-        return obj.username == request.user or request.user.is_superuser
+    def has_object_permission(self, request, view, obj):
+        return obj.id == request.user.id or request.user.is_superuser
