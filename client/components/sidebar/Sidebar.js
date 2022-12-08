cc213430afb73d145239f766286183e75a560321
Sebastian Tarczyński <starczynski7@gmail.com>
2022-12-08 17:44:27 +0100

    refactor sidebar

--- client/components/sidebar/Sidebar.js
+++ client/components/sidebar/Sidebar.js
@@ -1,110 +1,48 @@
-import { useState, useEffect, useCallback } from "react";
-import { MdOutlineDoubleArrow, MdMessage } from "react-icons/md";
-import { BsThreeDotsVertical } from "react-icons/bs";
-import Image from "next/image";
-import Dropdown from "../Dropdown";
-import Searchbar from "./Searchbar";
+import { useState } from "react";
+import { MdOutlineDoubleArrow } from "react-icons/md";
+import Header from "./Header";
 import Conversations from "./Conversations";
-import { useDispatch } from "react-redux";
-import { logout } from "../../features/authSlice";
+import NewConversation from "./NewConversation";
+import useDetectScreen from "../../hooks/UseDetectScreen";
 const Sidebar = () => {
-  const dispatch = useDispatch();
-
-  const actions = [
-    {
-      action: "New group",
-      func: () => console.log("New group"),
-    },
-    {
-      action: "Settings",
-      func: () => console.log("Settings"),
-    },
-    {
-      action: "Logout",
-      func: () => dispatch(logout()),
-    },
-  ];
-
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const [isMobile, setIsMobile] = useState(false);
-  const [newConversation, setNewConversation] = useState(false);
+  const [isNewConversationOpen, setIsNewConversationOpen] = useState(false);
+  const isSmallScreen = useDetectScreen(1024);
 
-  const detectMobileScreen = useCallback(() => {
-    const isActive = window.innerWidth < 1024;
-    setIsMobile(isActive);
-  }, [setIsMobile]);
+  const openSidebarClasses = `fixed lg:relative h-full flex flex-col bg-primary w-5/6 lg:w-[400px] border-r border-stone-100/20 z-50 ${
+    isSmallScreen
+      ? isSidebarOpen
+        ? "translate-x-0"
+        : "-translate-x-full"
+      : null
+  } transition-transform duration-300`;
 
-  useEffect(() => {
-    detectMobileScreen();
-    window.addEventListener("resize", detectMobileScreen);
-    return () => window.removeEventListener("resize", detectMobileScreen);
-  }, [detectMobileScreen]);
+  const openSidebarBtnClasses =
+    "absolute top-2/4 right-[-30px] h-[30px] w-[30px] translate-y-1/2 rounded-full cursor-pointer text-stone-400";
 
-  const openSidebarHandler = () => {
-    setIsSidebarOpen((prevState) => !prevState);
-  };
-
-  const openDropdownHandler = () => {
-    setIsDropdownOpen((prevState) => !prevState);
-  };
+  const openSidebarBtn = isSmallScreen && (
+    <span
+      className={openSidebarBtnClasses}
+      onClick={() => setIsSidebarOpen((prev) => !prev)}
+    >
+      <MdOutlineDoubleArrow
+        className={`${isSidebarOpen ? "rotate-180" : "rotate-0"} text-3xl`}
+      />
+    </span>
+  );
 
   return (
-    <div
-      className={`fixed lg:relative h-full flex flex-col bg-primary w-5/6 lg:w-[400px] border-r border-stone-100/20 z-50 ${
-        isMobile
-          ? isSidebarOpen
-            ? "translate-x-0"
-            : "-translate-x-full"
-          : null
-      } transition-transform duration-300`}
-    >
-      <div className="flex p-5 justify-between bg-secondary text-stone-100">
-        <div className="w-10 h-10 rounded-full cursor-pointer">
-          <Image
-            layout="responsive"
-            width="100%"
-            height="100%"
-            className="rounded-full object-cover"
-            src="/avatar.jpeg"
-          />
-        </div>
-        <div className="flex space-x-2.5">
-          <div
-            className="p-2 rounded-full cursor-pointer"
-            onClick={() => setNewConversation(true)}
-          >
-            <span className="text-2xl">
-              <MdMessage />
-            </span>
-          </div>
-          <div
-            className={`relative p-2 rounded-full cursor-pointer ${
-              isDropdownOpen ? "bg-primary" : null
-            }`}
-            onClick={openDropdownHandler}
-          >
-            <span className="text-2xl">
-              <BsThreeDotsVertical />
-            </span>
-            <Dropdown actions={actions} isOpen={isDropdownOpen} />
-          </div>
-        </div>
-      </div>
-
-      {/* <Searchbar /> */}
-      {newConversation ? <UsersList /> : <Conversations />}
-
-      {isMobile && (
-        <span
-          className="absolute top-2/4 right-[-30px] h-[30px] w-[30px] translate-y-1/2 rounded-full cursor-pointer text-stone-400"
-          onClick={openSidebarHandler}
-        >
-          <MdOutlineDoubleArrow
-            className={`${isSidebarOpen ? "rotate-180" : "rotate-0"} text-3xl`}
-          />
-        </span>
+    <div className={openSidebarClasses}>
+      <Header
+        openNewConversation={setIsNewConversationOpen}
+        isNewConversationOpen={isNewConversationOpen}
+      />
+      {isNewConversationOpen ? (
+        <NewConversation open={setIsNewConversationOpen} />
+      ) : (
+        <Conversations />
       )}
+      {openSidebarBtn}
     </div>
   );
 };
