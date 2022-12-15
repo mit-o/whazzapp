a0363e3dab4e13d66c5e408c298ebe6fef805c04
Sebastian Tarczyński <starczynski7@gmail.com>
2022-12-15 11:20:44 +0100

    add firebase storage helper

---
+++ backend/lib/firebase_storage.py
@@ -0,0 +1,30 @@
+from core.settings import firebase_storage, firebase_user
+from secrets import token_hex
+
+MAX_AVATAR_SIZE = 1024 * 1024 * 2
+
+
+class FirebaseStorageHelper:
+    def __init__(self, entity_id, path_prefix):
+        self.entity_id = entity_id
+        self.path_prefix = path_prefix
+        self.files = firebase_storage.list_files()
+
+    def delete_old_avatars(self):
+        old_avatars = [
+            av
+            for av in self.files
+            if av is not None
+            and av.name.startswith(f"{self.path_prefix}/{self.entity_id}_")
+        ]
+        if len(old_avatars) > 0:
+            for old_avatar in old_avatars:
+                firebase_storage.delete(old_avatar.name, firebase_user["idToken"])
+
+    def upload_avatar(self, avatar_file):
+        file_hash = f"{self.entity_id}_{token_hex(4)}"
+        path_to_avatar = f"{self.path_prefix}/{file_hash}"
+        firebase_storage.child(path_to_avatar).put(
+            avatar_file, firebase_user["idToken"]
+        )
+        return firebase_storage.child(path_to_avatar).get_url(None)
